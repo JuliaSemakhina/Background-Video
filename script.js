21b45e5d1075e1a4043dc940c715eed496919ecf
Julia Semakhina <contremoi@yandex.ru>
2021-01-07 21:04:49 +0300

    final files

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let wrapper = document.getElementById('wrapper');
-    let topLayer = wrapper.querySelector('.top');
-    let handle = wrapper.querySelector('.handle');
-    let skew = 0;
-    let delta = 0;
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
 
-
-    if (wrapper.className.indexOf('skewed') != -1) {
-        skew = 990;
-    }
-
-    wrapper.addEventListener('mousemove', function(e) {
-        delta = (e.clientX - window.innerWidth / 2) * 0.5;
-
-        handle.style.left = e.clientX + delta + "px";
-
-        topLayer.style.width = e.clientX + skew + delta + "px";
-    })
-
-
-});
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+})
